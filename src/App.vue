9afaae2800118c5f2e2d9daffdfa25a189f33dda
Amber <amber20240@gmail.com>
2020-06-10 20:24:19 +0800

    counter

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+  <h2>counter</h2>
+  <p>{{count}}</p>
+  <button @click="add">+1</button>    
+  </div>
+</template>
+
+<script>
+export default{
+  computed:{
+    count(){
+      return this.$store.state.count
+    }
+  },
+  methods:{
+    add(){
+      this.$store.commit("counter")
+    }
+  }
+} 
+</script>
